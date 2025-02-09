1a210102e8416ed042e91f2052b86f94af9a64d1
Tursunboy Soyibjonov <soyibjonovtursunboy.zero.one@gmail.com>
2025-02-09 19:28:05 +0500

    initila commit

---
+++ src/calc.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, test } from 'vitest';
+
+describe('Faqat shu testlar ishlaydi', () => {
+  test('Test 1', () => {
+    expect(1 + 1).toBe(2);
+  });
+
+  test('Test 2', () => {
+    expect(2 * 2).toBe(4);
+  });
+});
+
+describe('Bu testlar ishlamaydi', () => {
+  test('Test 3', () => {
+    expect(3 + 3).toBe(6);
+  });
+});
